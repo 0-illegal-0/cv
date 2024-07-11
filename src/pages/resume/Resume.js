073fa8c6d4c09a921d6d30209b4bf5ea4ef77fa9
soliman <solimanonline0@gmail.com>
2024-07-11 06:54:20 +0300

    skills section

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Resume() {
   return (
@@ -49,6 +49,7 @@ function Resume() {
             date={"2028 - 2020"}
           />
         </div>
+        <Skills />
       </div>
     </div>
   );
@@ -96,6 +97,82 @@ function ExperienceSection({ qualification, university, date }) {
         tincidunt augue in leo feugiat, quis interdum nisi sollicitudin, quis
         interdum nisi sollicitudin
       </p>
+    </div>
+  );
+}
+
+const skillsValues = [
+  { value: 80, "skill-name": "HTML" },
+  { value: 75, "skill-name": "CSS" },
+  { value: 90, "skill-name": "JAVASCRIPT" },
+  { value: 95, "skill-name": "PHP" },
+  { value: 90, "skill-name": "ANGULAR" },
+  { value: 80, "skill-name": "REACT" },
+  { value: 85, "skill-name": "MYSQL" },
+  { value: 90, "skill-name": "LARAVEL" },
+];
+
+function Skills() {
+  function kol() {
+    skillsValues.forEach((element) => {
+      return <h2>ho</h2>;
+    });
+  }
+  return (
+    <div className="skills">
+      <h3>Skills</h3>
+      <div className="skills-container">
+        {skillsValues.map((count) => {
+          return (
+            <SkillValue
+              value={count["value"]}
+              skillName={count["skill-name"]}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+let ctx;
+let canvas;
+let keyLengthIndex = 0;
+
+function SkillValue({ value, skillName }) {
+  const [keyLength, SetkeyLength] = useState(value);
+
+  function circleo() {
+    if (document.getElementById(skillName)) {
+      console.log("---- ", value);
+      canvas = document.getElementById(skillName);
+      ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.arc(80, 80, 76, 0, ((2 * value) / 100) * Math.PI);
+      ctx.lineWidth = 7;
+      ctx.strokeStyle = "#444764"; //"#9425b95e";
+
+      ctx.stroke();
+    }
+  }
+
+  useEffect(() => {
+    circleo();
+  });
+
+  return (
+    <div className="main-circle">
+      <canvas
+        className="skill-circle-canvas"
+        id={skillName}
+        width="160"
+        height="160"
+      ></canvas>
+      <div className="small-circle">
+        <div className="skill-circle">
+          <h3>{value}%</h3>
+        </div>
+      </div>
     </div>
   );
 }
